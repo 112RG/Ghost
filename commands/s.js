821d8d63d62bae6b8d66d3098a0d7a2ce5ccf73b
Rylee George <ryleegeorge1@gmail.com>
2017-05-01 22:03:22 +1000

    Update from Kuro

--- commands/s.js
+++ commands/s.js
@@ -3,73 +3,87 @@ let _msg
 let _stickers = {}
 let _table = 'stickers'
 let assets = './files/stickers'
-let fs
+const fs = require('fs')
+const axios = require('axios')
+const FormData = require('form-data')
+const concat = require('concat-stream')
 
-exports.init = function(bot){ 
+exports.init = function(bot) {
 	ghost = bot
-	fs = require('fs')
 
 	// Create sticker folder if it doesn't exist
 	fs.existsSync(assets) || fs.mkdirSync(assets)
 
 	// Create the table where we will be storing this module's data
-	bot.db.schema.createTableIfNotExists(_table, function (table) {
+	bot.db.schema.createTableIfNotExists(_table, (table) => {
 		table.increments()
 		table.string('name')
 		table.string('file')
-	}).then(function () {
-		
+		table.string('url')
+	}).then(() => {
 		// Lets load up the existing stickers
-		bot.db.table(_table).then(function(rows){
-			for(let row of rows)
-				_stickers[row.name] = row.file
+		bot.db.table(_table).then((rows) => {
+			for (let row of rows) {
+				if (row.url !== '' && row.url !== undefined) {
+					_stickers[row.name] = row.url
+				} else {
+					_stickers[row.name] = row.file
+				}
+			}
 		})
-
-	}).catch(function(error) { ghost.error(error) })
+	})
+	.catch((error) => { ghost.error(error) })
 }
 
 exports.run = function(msg, args) {
-
+	console.log(args)
 	_msg = msg
 
-	if(!(args instanceof Array)){
-		if(_stickers.hasOwnProperty(args)) return this.sendSticker(args)
+	if (!(args instanceof Array)) {
+		if (_stickers.hasOwnProperty(args)) return this.sendSticker(args)
 		return _msg.delete()
 	}
 
-	let newargs  = []
-	for(let i = 1; i < args.length; i++)
+	let newargs = []
+	for (let i = 1; i < args.length; i++) {
 		newargs.push(args[i])
+	}
 
 	// Subcommand?
-	if(args[0] === 'add') return this.add(newargs)
-	if(args[0] === 'del') return this.del(newargs)
-	if(args[0] === 'ren') return this.ren(newargs)
-	if(args[0] === 'list') return this.list()
-	if(args[0] === 'migrate') return this.migrate()
+	if (args[0] === 'add') return this.add(newargs)
+	if (args[0] === 'del') return this.del(newargs)
+	if (args[0] === 'ren') return this.ren(newargs)
+	if (args[0] === 'list') return this.list()
+	if (args[0] === 'migrate') return this.migrate()
 
 	// Not a subcommand, let's see if it's a sticker
-	if(_stickers.hasOwnProperty(args[0])) return this.sendSticker(args[0])
-	
+	if (_stickers.hasOwnProperty(args[0])) return this.sendSticker(args[0])
+
 	// Ded
 	_msg.delete()
-	
 }
 
-exports.sendSticker = function(name){
-	
-	let file = assets + '/' + _stickers[name]
-	fs.access(file, fs.constants.R_OK, (err) => {
-		if(err) return _msg.edit('**Error:**\n' + err)
-		
-		_msg.delete()
-		let img = fs.readFileSync(file)
-		_msg.channel.sendFile(img, _stickers[name])
-	})
+exports.sendSticker = function(name) {
+	if (_stickers[name].startsWith('http')) {
+		_msg.edit({
+			embed: {
+				image: { url: 'https://lolisafe.moe/gQ7eQGym.jpg' }
+			}
+		});
+	} else {
+		let file = `${assets}/${_stickers[name]}`
+		fs.access(file, fs.constants.R_OK, (err) => {
+			if (err) return _msg.edit(`**Error:**\n${err}`)
+
+			_msg.delete()
+			let img = fs.readFileSync(file)
+			return _msg.channel.sendFile(img, _stickers[name])
+		})
+	}
 }
 
-exports.add = function(args){
-	if(args[0] === undefined){
+exports.add = function(args) {
+	if (args[0] === undefined) {
 		ghost.edit(_msg, 'No name provided.')
 		return
 	}
@@ -77,26 +91,29 @@ exports.add = function(args){
 	let name = args[0]
 
 	// Is the name of the sticker already used?
-	if(_stickers.hasOwnProperty(name)){
+	if (_stickers.hasOwnProperty(name)) {
 		ghost.edit(_msg, 'Name already in use.')
 		return
 	}
 
 	// Prepare the destination container
-	let dest = assets + '/' + name
+	let dest = `${assets}/${name}`
 	let url = ''
+
 	// Stupid discord renaming stuff, breaks everything
 	let discordFilename = ''
-	if(args[1] !== undefined)
+	if (args[1] !== undefined) {
 		url = args[1]
-	else
-		if( typeof _msg.attachments.first() !== 'undefined'){
-			if('proxyURL' in _msg.attachments.first())
+	} else {
+		if (typeof _msg.attachments.first() !== 'undefined') {
+			if ('proxyURL' in _msg.attachments.first()) {
 				url = _msg.attachments.first().proxyURL
 				discordFilename = _msg.attachments.first().filename
 			}
+		}
+	}
 
-	if(url === ''){
+	if (url === '') {
 		// Welp, couldn't figure out a url
 		ghost.edit(_msg, 'You didnt supply either a url nor attachment, or there was an error with the attachment.')
 		return
@@ -106,151 +123,166 @@ exports.add = function(args){
 	let re = /(?:\.([^.]+))?$/
 	let ext = re.exec(url)[1]
 
-	if(discordFilename !== '')
+	if (discordFilename !== '') {
 		ext = re.exec(discordFilename)[1]
+	}
 
-	if(ext === undefined){
+	if (ext === undefined) {
 		ghost.edit(_msg, 'The file you are linking or trying to attach doesn\'t have an extension. ghost needs that thingy. pls fam')
 		return
 	}
 
-	dest = dest + '.' + ext
+	dest = `${dest}.${ext}`
 	this.downloadImage(name, url, dest, ext)
 }
 
-exports.del = function(args){
-	if(args[0] === undefined) return ghost.edit(_msg, 'No name provided.')
-	
-	if(args[0] in _stickers){
-		ghost.db.table(_table).where('name', args[0]).del().then(function(){
-			delete(_stickers[args[0]])
-			return ghost.edit(_msg, 'The sticker was removed.', 1000)
-		}).catch(function(e){ ghost.edit(_msg, 'Error: \n' + e, 0)})
-	}else{
+exports.del = function(args) {
+	if (args[0] === undefined) return ghost.edit(_msg, 'No name provided.')
+
+	if (args[0] in _stickers) {
+		ghost.db.table(_table)
+			.where('name', args[0])
+			.del()
+			.then(() => {
+				delete (_stickers[args[0]])
+				return ghost.edit(_msg, 'The sticker was removed.', 1000)
+			})
+			.catch((e) => { ghost.edit(_msg, `Error: \n${e}`, 0) })
+	} else {
 		return ghost.edit(_msg, 'There is no sticker by that name.')
 	}
 }
 
-exports.ren = function(args){
-	if(args[0] === undefined) return ghost.edit(_msg, 'No source sticker supplied.')
-	if(args[1] === undefined) return ghost.edit(_msg, 'No destination sticker supplied.')
-	
-	if(args[0] in _stickers){
-		
-		ghost.db.table(_table).where('name', args[0]).update({
-			name: args[1]
-		}).then(function(){
-			_stickers[args[1]] = _stickers[args[0]]
-			delete(_stickers[args[0]])
-			return ghost.edit(_msg, 'Sticker renamed.', 1000)
-		}).catch(function(e){ ghost.edit(_msg, 'Error: \n' + e, 0)})
+exports.ren = function(args) {
+	if (args[0] === undefined) return ghost.edit(_msg, 'No source sticker supplied.')
+	if (args[1] === undefined) return ghost.edit(_msg, 'No destination sticker supplied.')
 
-	}else{
+	if (args[0] in _stickers) {
+		ghost.db.table(_table)
+			.where('name', args[0])
+			.update({ name: args[1] })
+			.then(() => {
+				_stickers[args[1]] = _stickers[args[0]]
+				delete (_stickers[args[0]])
+				return ghost.edit(_msg, 'Sticker renamed.', 1000)
+			})
+			.catch((e) => { ghost.edit(_msg, `Error: \n${e}`, 0) })
+	} else {
 		return ghost.edit(_msg, 'There is no sticker by that name.')
 	}
 }
 
-exports.list = function(){
-	if(ghost.config.server.enabled === true)
-		this.startServer()
-	else{
-		let list = ''
-		for (let sticker in _stickers)
-			if ({}.hasOwnProperty.call(_stickers, sticker))
-				list = list + sticker + ', '
-
-		list = list.substr(0, list.length - 2)
-		return ghost.edit(_msg, `**__Stickers list__**\n\`\`\`\n${list}\n\`\`\``, 10000)
+exports.list = function() {
+	let list = ''
+	for (let sticker in _stickers) {
+		if ({}.hasOwnProperty.call(_stickers, sticker)) {
+			list = `${list}${sticker}, `
+		}
 	}
+
+	list = list.substr(0, list.length - 2)
+	return ghost.edit(_msg, `**__Stickers list__**\n\`\`\`\n${list}\n\`\`\``, 10000)
 }
+
 
 exports.downloadImage = function(name, url, dest, ext) {
 	let saveFile = require('request')
 		.get(url)
 		.on('error', (err) => {
 			ghost.log(err)
-			_msg.edit('***Error:*** ' + err)
+			_msg.edit(`***Error:*** ${err}`)
 		})
 		.pipe(fs.createWriteStream(dest))
 
-	saveFile.on('finish', () => { 
-		ghost.db.table(_table).insert({
-			name: name,
-			file: name + '.' + ext
-		}).then(function(){
-			_stickers[name] = name + '.' + ext
-			ghost.edit(_msg, 'Sticker added', 1000)
-		}).catch(function(e){ ghost.edit(_msg, 'Error: \n' + e, 0)})
+	saveFile.on('finish', () => {
+		this.uploadImage(name, dest, ext)
 	})
 }
 
-exports.migrate = function(){
+exports.uploadImage = function(name, dest, ext) {
+	const fd = new FormData()
 
-	try{
-		let oldfolder = './stickers'
-		let newfolder = assets
+	fd.append('files[]', fs.createReadStream(dest))
+	fd.pipe(concat({ encoding: 'buffer' }, data => {
+		axios.post('https://safe.moe/api/upload', data, { headers: fd.getHeaders() })
+		.then((response) => {
+			if (response.data.success === false) {
+				console.error('Error uploading to safe.moe: ', response.description)
+				ghost.error('Error saving sticker. Check logs')
+				return this.saveStickerToDB(name, ext, '')
+			}
 
-		const fs = require('fs')
+			return this.saveStickerToDB(name, ext, response.data.files[0].url)
+		})
+		.catch((err) => console.log(err))
+	}))
+}
 
-		if(!fs.existsSync(oldfolder)) return ghost.edit(_msg, 'There doesn\'t seem to be an old sticker folder')
+exports.saveStickerToDB = function(name, ext, url) {
+	ghost.db.table(_table).insert({
+		name: name,
+		file: `${name}.${ext}`,
+		url: url
+	})
+	.then(() => {
+		_stickers[name] = `${name}.${ext}`
+		if (url !== '') _stickers[name] = url
+		ghost.edit(_msg, 'Sticker added', 1000)
+	})
+}
 
-		fs.readdir(oldfolder, (err, files) => {
-			files.forEach(file => {
-				
-				// Seems we found some files. Let's asume they are stickers :araragi:
-				let name = file.slice(0, -4)
+exports.migrate = function() {
+	console.log('Starting migration')
+	_msg.edit('*Starting migration to ghost v4.1.0, this might take a while depending how many stickers you have. Check the console.*')
 
-				// Is the name of the sticker already used?
-				if(!_stickers.hasOwnProperty(name)){
+	try {
 
-					copyFile(oldfolder + '/' + file, newfolder + '/' + file)
+		ghost.db.schema.table(_table, (table) => {
+			table.string('url')
+		}).then(() => {
+			// Done for now
+			ghost.db.table(_table).then((rows) => {
+				console.log('Found ' + rows.length + ' stickers, starting the upload to safe.moe')
 
-					ghost.db.table(_table).insert({
-						name: name,
-						file: file
-					}).then(function(){
-						_stickers[name] = file
-						ghost.log('Migrated ' + file)
-					}).catch((e) => ghost.error(e))	
+				let counter = 1
+				for (let row of rows) {
+					if (row.url === '' || row.url === undefined || row.url === null) {
+						const fd = new FormData()
+
+						fd.append('files[]', fs.createReadStream(`${assets}/${row.file}`))
+						fd.pipe(concat({ encoding: 'buffer' }, data => {
+							axios.post('https://safe.moe/api/upload', data, { headers: fd.getHeaders() })
+							.then((response) => {
+								if (response.data.success === false) {
+									console.error('Error uploading to safe.moe: ', response.description)
+								} else {
+									ghost.db.table(_table).where('id', row.id).update(
+										{ url: response.data.files[0].url }
+									)
+									.then(() => {
+										console.log('Finished uploading ' + counter + '/' + rows.length + ': ' + row.name)
+										counter++
+										_stickers[row.name] = response.data.files[0].url
+										if (counter > rows.length) {
+											console.log('Migration finished, you can keep using ghost like you normally would')
+											_msg.edit('*Migration finished, you can keep using ghost like you normally would*')
+										}
+									})
+								}
+							})
+							.catch((err) => console.log(err))
+						}))
+					}
 				}
-
 			})
 		})
 
-		_msg.edit('Migration finished. Check console for logs.')
-
-	}catch(e){
+		.catch((error) => { ghost.error(error) })
+	} catch (e) {
 		ghost.error(e)
 	}
 }
 
-exports.stickerCount = function(){
+exports.stickerCount = function() {
 	return Object.keys(_stickers).length
 }
-
-exports.startServer = function(){}
-
-function copyFile(source, target) {
-	var cbCalled = false
-
-	var rd = fs.createReadStream(source)
-	rd.on('error', function(err) {
-		done(err)
-	})
-	var wr = fs.createWriteStream(target)
-	wr.on('error', function(err) {
-		done(err)
-	})
-	wr.on('close', function(ex) {
-		done()
-	})
-	rd.pipe(wr)
-
-	function done(err) {
-		if (!cbCalled) {
-			if(err) ghost.error(err)
-			cbCalled = true
-		}
-	}
-}
-
